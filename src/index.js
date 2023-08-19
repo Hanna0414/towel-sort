49fba6a071cd81ab468f115bbf20cfa91306c51f
Hanna0414 <nytelya408@gmail.com>
2023-08-20 02:55:16 +0300

    feat:add towel sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 
 // You should implement your task here.
 
+const { sort } = require("semver");
+
 module.exports = function towelSort (matrix) {
-  return [];
-}
+  let result = [];
+  
+  if (matrix === undefined || matrix.length === 0)
+  {return result;}
+  else {
+    for (let i=0; i<matrix.length; i++) {
+        if (i%2 == 0) {
+            for ( j=0; j<matrix[i].length; j++) {
+                result.push(matrix[i][j]);
+            }
+        } else {
+            for (j=matrix[i].length-1; j>-1; j--) {
+                result.push(matrix[i][j]);
+        }
+    }
+    }
+    return result;
+  }
+};
